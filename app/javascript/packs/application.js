c8531f85e61a46399d3776ecf24a6411deeba594
ttr42hozu <shigi10nato@gmail.com>
2020-12-21 15:29:08 +0900

    AjaxAppの準備

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../checked")
+// checked.jsを読み込むための記述
+require("../memo")
+// memo.jsを読み込むための記述
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
