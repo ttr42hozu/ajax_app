c82ed401590612235bfcde319f87927f6428dec2
ttr42hozu <shigi10nato@gmail.com>
2020-12-21 16:38:17 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,19 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    post = Post.find(params[:id])
+    if post.checked 
+    # checkedプロパティはチェックボックスが選択されていればtrueを、そうでなければfalseを返す
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
+    # item = Post.find(params[:id])で取得し直し、render json:{ post: item }でJSON形式（データ）としてchecked.jsに返却
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
+  # 発展3-8 posts#checkedは何を示している？
+  # コントローラーではなくchecked.jsにリクエストを振り分けている？
 end
